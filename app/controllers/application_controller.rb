26ead03708a19fcbd5cdc6525ae7773d6344915d
dmitryS1666 <dsuschinsky@gmail.com>
2024-04-13 00:49:33 +0300

    fix config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,9 @@ class ApplicationController < ActionController::Base
   before_action :set_main_category
   before_action :set_popular_product
   before_action :set_top_rated_product
-  before_action do
-    ActiveStorage::Current.host = ENV["DOMAIN"] || "localhost:3000"
-  end
+  # before_action do
+  #   ActiveStorage::Current.host = 'agromaster.dsml.ru'
+  # end
 
   def set_render_cart
     @render_cart = true
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def default_url_options
-    { host: ENV["DOMAIN"] || "localhost:3000" }
-  end
+  # def default_url_options
+  #   { host: ENV["DOMAIN"] || "localhost:3000" }
+  # end
 end
