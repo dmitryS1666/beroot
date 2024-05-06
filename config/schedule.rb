5342ed996366895c5cfbc9065207f56403aa8321
d.suschinsky <d.suschinsky@dsml.ru>
2024-05-06 10:27:48 +0300

    fix scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,6 @@
 set :output, "log/cron.log"
-set :bundle_command, "/home/u/.rbenv/shims/bundle exec"
+# set :bundle_command, "/home/u/.rbenv/shims/bundle exec"
+set :bundle_command, "/home/deploy/.rbenv/shims/bundle exec"
 
 # every 1.day, at: '23:30 pm' do
 #   rake 'update_products:update_db'
