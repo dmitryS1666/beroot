4cc67ec23771e9fb1fdc4cbbc28974de7d3fcb1e
d.suschinsky <d.suschinsky@dsml.ru>
2024-05-07 00:30:38 +0300

    fix watermartk

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,10 +2,10 @@ set :output, "log/cron.log"
 # set :bundle_command, "/home/u/.rbenv/shims/bundle exec"
 set :bundle_command, "/home/deploy/.rbenv/shims/bundle exec"
 
-# every 1.day, at: '23:30 pm' do
-#   rake 'update_products:update_db'
-# end
-
-every 5.minutes do
+every 1.day, at: '23:30 pm' do
   rake 'update_products:update_db'
 end
+
+# every 5.minutes do
+#   rake 'update_products:update_db'
+# end
